703713ccaded4bc791dfb382d81b94edecf20e99
shpeeck <jenya_vas@mail.ru>
2020-07-04 20:17:08 +0300

    Работающий парсер котегорий и подкатегорий

--- prj/backend/management/commands/load_category.py
+++ prj/backend/management/commands/load_category.py
@@ -1,6 +1,50 @@
+from django.core.management.base import BaseCommand, CommandError
+from backend.models import Category, Subcategory
+
 from requests_html import HTMLSession
 
 
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        print('Start')
+        Category.objects.all().delete()
+        Subcategory.objects.all().delete()
+
+        session = HTMLSession()
+        resp = session.get('http://west-info.ua/katalog-predpriyatij/')
+        a = 1
+
+        while True:
+            b = 1
+            try:
+                links = resp.html.xpath(f'/html/body/header/div[3]/div/nav/ul/li[2]/ul/li[{a}]/a/text()')
+                print('Категория: {}'.format(links[0]))
+                cat = Category()
+                cat.name = links[0]
+                cat.save()
+                while True:
+                    try:
+                        links = resp.html.xpath(f'/html/body/header/div[3]/div/nav/ul/li[2]/ul/li[{a}]/ul/li[{b}]/a/text()')
+                        print('Подкатегория -- {}'.format(links[0]))
+                        sub = Subcategory()
+                        sub.name = links[0]
+                        sub.category = cat
+                        sub.save()
+                        b += 1 
+                    except:
+                        break
+                a += 1 
+            except:
+                break
+
+
+
+
+
+
+
+'''
 session = HTMLSession()
 resp = session.get('http://west-info.ua/katalog-predpriyatij/')
 
@@ -25,3 +69,4 @@ while True:
         break
 
 
+'''
