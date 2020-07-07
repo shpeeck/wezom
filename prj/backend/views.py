12d8df66a6ab7e4fa0259a13a22da004751a073c
shpeeck <jenya_vas@mail.ru>
2020-07-07 21:15:03 +0300

    Добавил пагинацию

--- prj/backend/views.py
+++ prj/backend/views.py
@@ -7,7 +7,19 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def index(request):
-    news = Enterprises.objects.all()
+    newses = Enterprises.objects.all()
+
+    # paginator
+    paginator = Paginator(newses, 16)
+    page = request.GET.get('page')
+    try:
+        news = paginator.page(page)
+    except PageNotAnInteger:
+        news = paginator.page(1)
+    except EmptyPage:
+        news = paginator.page(paginator.num_pages)
+    # end paginator
+
     cat = Category.objects.all()
     subcat = Subcategory.objects.all()
     return render(request, 'frontend/index.html', {'news': news, 'cat': cat, 'subcat': subcat})
@@ -21,7 +33,19 @@ def contact(request):
     return render(request, 'frontend/contact.html', {'news': news})
 
 def get_category(request, category_id):
-    news = Enterprises.objects.filter(subcategory=category_id)
+    newses = Enterprises.objects.filter(subcategory=category_id)
+
+    # paginator
+    paginator = Paginator(newses, 16)
+    page = request.GET.get('page')
+    try:
+        news = paginator.page(page)
+    except PageNotAnInteger:
+        news = paginator.page(1)
+    except EmptyPage:
+        news = paginator.page(paginator.num_pages)
+    # end paginator
+
     cat = Category.objects.all()
     subcat = Subcategory.objects.all()
     subctig = Subcategory.objects.get(pk=category_id)
